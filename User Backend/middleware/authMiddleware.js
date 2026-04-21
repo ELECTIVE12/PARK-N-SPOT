226860b4d8593ae7d8582e0aba9076db1446590b
CPE3C-abajo-kylle <2023104823@ms.bulsu.edu.ph>
2026-04-21 14:01:44 +0800

    Update authMiddleware.js

--- User Backend/middleware/authMiddleware.js
+++ User Backend/middleware/authMiddleware.js
@@ -4,19 +4,19 @@ const User = require('../models/User');
 const protect = async (req, res, next) => {
   let token;
 
-  if (req.headers.authorization?.startsWith('Bearer')) {
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
       token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select('-password');
       next();
-    } catch {
-      return res.status(401).json({ message: 'Not authorized, invalid token' });
+    } catch (err) {
+      res.status(401).json({ message: 'Not authorized, token failed' });
     }
   }
 
   if (!token) {
-    return res.status(401).json({ message: 'Not authorized, no token' });
+    res.status(401).json({ message: 'Not authorized, no token' });
   }
 };
 
